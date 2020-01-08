fe6f5d2b1725e1be06f628b9245aefbc095756e2
Florian Dambrine <florian@gumgum.com>
2020-01-08 05:58:02 -0800

    Init repo

--- lib/glow/effects/breath_dim_effect.py
+++ lib/glow/effects/breath_dim_effect.py
@@ -10,14 +10,26 @@ logger = logging.getLogger()
 
 class BreathDimEffect(DimEffect):
     def __init__(
-        self, name: str, brightness: int = 127, step: int = 10, clockwise: bool = True
+        self,
+        name: str,
+        brightness: int = 127,
+        step: int = 10,
+        clockwise: bool = True,
+        smooth: bool = True,
     ):
         super().__init__(name, brightness=brightness)
         self._step = step
         self._clockwise = -1 if clockwise else +1
+        self._smooth = smooth
 
     def _breath(self):
-        self._brightness = (self._brightness + (self._step * self._clockwise)) % 255
+        new_brightness = self._brightness + (self._step * self._clockwise)
+        # If smooth play tick tock effect with clockwise
+        if self._smooth:
+            if new_brightness < 0 or new_brightness > 255:
+                self._clockwise = not self._clockwise
+
+        self._brightness = new_brightness % 255
 
     def dim(self) -> int:
         self._breath()
